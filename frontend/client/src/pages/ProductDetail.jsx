19a36fb10767fd631b59428415f9473fe256f4c3
Agustin Galvan <93547064+Agustingalvan02@users.noreply.github.com>
2023-05-16 16:35:17 -0300

    New branch

--- frontend/client/src/pages/ProductDetail.jsx
+++ frontend/client/src/pages/ProductDetail.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+  return <div>ProductDetail
+  <div></div>
+  </div>
+  ;
 };
 
 export default ProductDetail;
