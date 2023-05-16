edeb6bd9b087320588d742585052d0e382156037
vic-ferr <victorhfp2014@gmail.com>
2023-05-15 22:25:28 -0400

    fixed configuration tailwind and add prototype hero componets

---
+++ frontend/client/src/components/Hero/HeroCategory/HeroCategory.jsx
@@ -0,0 +1,27 @@
+import electrodomestic from "../../../assets/img/electrodomestic.png";
+import phone from "../../../assets/img/phone.png";
+import shoes from "../../../assets/img/shoes.png";
+import house from "../../../assets/img/house.png";
+const HeroCategory = () => {
+  return (
+    <div className="bg-black">
+      <h2 className=" text-center text-3xl font-bold"> EXPLORÁ POR CATEGORÍA </h2>
+      <div className="flex flex-row">
+        <article className="">
+          <img src={electrodomestic} alt="electrodomestic" />
+        </article>
+        <article className="">
+          <img src={phone} alt="phone" />
+        </article>
+        <article className="">
+          <img src={shoes} alt="shoes" />
+        </article>
+        <article className="">
+          <img src={house} alt="house" />
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default HeroCategory;
