98787940ea9e59feeb9f155c66f00f9d73156157
vic-ferr <victorhfp2014@gmail.com>
2023-05-17 23:22:53 -0400

    add prototype Sales

--- frontend/client/src/pages/Home.jsx
+++ frontend/client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import Sales from "../components/Sales/Sales";
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-[#EDEDED]">
       Home123
-      <section>
+      <section className="mx-auto w-[85%]">
         <Hero />
+      </section>
+      <section className="mx-auto w-[85%]">
+        <Sales />
       </section>
     </div>
   );
