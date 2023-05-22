e7cbeb9d46052eb3d7241733a8688d522bab60f9
Brajhan Lopez <102929742+BrajhanLop@users.noreply.github.com>
2023-05-22 15:43:04 -0500

    add carditem and route paymethod (#18)

---
+++ frontend/client/src/components/Payments/CardItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardItem = ({children}) => {
+  return (
+    <div className=' flex items-center border bg-white xl:py-8 xl:px-10 xl:w-[750px] xl:gap-8 '>
+        {children}
+    </div>
+  )
+}
+
+export default CardItem
