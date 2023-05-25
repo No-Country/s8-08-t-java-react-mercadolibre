0632090485471a6a051bb71e2f6625bfa1f4e444
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-05-25 12:29:47 -0500

    add format

--- frontend/client/src/components/Payments/Loader.jsx
+++ frontend/client/src/components/Payments/Loader.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import payment from '../../assets/img/logo-payment.png'
+import payment from "../../assets/img/logo-payment.png";
 const Loader = () => {
   return (
     <div className="flex flex-col gap-3 justify-center items-center">
-      <div className="border-8 border-white border-t-sky-600 rounded-full w-16 h-16 animate-spin">
-        
-      </div>
+      <div className="border-8 border-white border-t-sky-600 rounded-full w-16 h-16 animate-spin"></div>
       <img src={payment} alt="" />
     </div>
   );
