7d2fee96c72c35773b99f0bfc60b3c88bb471d6e
Franco Buceta <francobuceta95@gmail.com>
2023-05-26 14:33:24 -0300

    add report problem icons

--- frontend/client/src/pages/Login.jsx
+++ frontend/client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import NavbarPayment from "../components/Payments/NavbarPayment";
 import account from "../assets/icons/account.png";
 import account2 from "../assets/icons/account2.png";
 import cellphone from "../assets/icons/cellphone.png";
+import arrow from "../assets/icons/arrow-right.png";
 
 const Login = () => {
   return (
@@ -27,6 +28,22 @@ const Login = () => {
                 <p>francobuceta95@gmail.com</p>
                 <p className="text-ligthblue">Cambiar cuenta</p>
               </div>
+            </div>
+
+            <div className="mt-10 w-[224px]">
+              <p className="font-medium text-[11px] mb-4">Reportar un problema</p>
+              <div className="flex items-center gap-2 text-[#0000008c] text-sm">
+                <img src={cellphone} alt="Icono de celular" />
+                <p>Robo o pérdida de teléfono</p>
+                <img src={arrow} alt="Flecha señalando hacia la derecha" className="ml-auto"/>
+              </div>
+              <hr className="my-2"/>
+              <div className="flex items-center gap-2 text-[#0000008c] text-sm ml-[-4px]">
+                <img src={account2} alt="Icono de persona" />
+                <p>Robo de cuenta</p>
+                <img src={arrow} alt="Flecha señalando hacia la derecha" className="ml-auto"/>
+              </div>
+              <p className="text-[13px] text-ligthblue mt-6">Necesito ayuda con otros temas</p>
             </div>
           </div>
 
