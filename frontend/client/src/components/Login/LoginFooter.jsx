23172bd4d525269b312b0f2f559385fb89f5896b
Franco Buceta <francobuceta95@gmail.com>
2023-05-27 10:51:35 -0300

    fix footer responsive

--- frontend/client/src/components/Login/LoginFooter.jsx
+++ frontend/client/src/components/Login/LoginFooter.jsx
@@ -2,15 +2,18 @@ const LoginFooter = () => {
   return (
     <div className=" bg-[#F5F5F5] h-[71px] sm:h-[56px] flex items-center">
       <footer className="flex w-full mx-8">
-        <div className="flex items-center justify-between w-full">
+        <div className="flex flex-col sm:flex-row items-center justify-between w-full">
           <div className="flex items-center gap-2 text-[11px]">
-            <span className="text-ligthblue">Cómo cuidamos tu privacidad</span>
-            <span className="text-[#0000008c]">Copyright © 1999-2023 MercadoLibre S.R.L.</span>
+            <p className="text-ligthblue">Cómo cuidamos tu privacidad</p>
+            <span className="text-[11px] text-[#0000008c]">-</span>
+            <p className="text-[#0000008c]">Copyright © 1999-2023 MercadoLibre S.R.L.</p>
           </div>
           <div className="flex items-center gap-1">
-            <span className="text-[12px] text-[#0000008c]">Protegido por reCAPTCHA</span>
-            <span className="text-[11px]">- Privacidad</span>
-            <span className="text-[11px]">- Condiciones</span>
+            <p className="text-[12px] text-[#0000008c]">Protegido por reCAPTCHA</p>
+            <span className="text-[11px] text-[#0000008c]">-</span>
+            <p className="text-[11px]">Privacidad</p>
+            <span className="text-[11px] text-[#0000008c]">-</span>
+            <p className="text-[11px]">Condiciones</p>
           </div>
         </div>
       </footer>
