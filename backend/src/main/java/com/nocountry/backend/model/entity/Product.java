dcde9a3cff561ee2baa646404af149cdca518da6
L Rayo <rayo862@hotmail.com>
2023-05-29 23:14:50 -0500

    fix create Subcategory

--- backend/src/main/java/com/nocountry/backend/model/entity/Product.java
+++ backend/src/main/java/com/nocountry/backend/model/entity/Product.java
@@ -35,5 +35,9 @@ public class Product {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne
+    @JoinColumn(name = "SubCategory_id")
+    private Subcategory subcategory;
+
 }
 
