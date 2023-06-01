022920a676ce9caf75faef865c2bf5c77226f77d
Dario Elguero <darioelguero@gmail.com>
2023-06-01 15:01:17 -0300

    add getAllCategories in Navbar component

--- frontend/client/src/components/Navbar/Navbar.jsx
+++ frontend/client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavbarSearch from "./NavbarSearch/NavbarSearch";
 import NavbarMenues from "./NavbarMenues/NavbarMenues";
 import useMediaQuery from "../../hooks/useMediaQuery";
 import MenuMobile from "./MenuMobile/MenuMobile";
+import { getAllCategories } from "../../store/state/categoriesSlice";
+import { useDispatch } from "react-redux";
 
 const Navbar = () => {
   const isMobile = useMediaQuery("(max-width: 640px)");
   const [openMenuMobile, setOpenMenuMobile] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log("USEEFECT");
+    dispatch(getAllCategories());
+  }, []);
+
+  const allCategories = () => {
+    getAllCategories();
+  };
+
   return (
     <nav className="flex flex-col items-center bg-[#FFF159] shadow w-full">
       <div className="flex flex-col items-center p-2 w-full sm:w-[90%]">
--- frontend/client/src/store/store.js
+++ frontend/client/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./state/authSlice";
+import categories from "./state/categoriesSlice";
 
 export const store = configureStore({
   reducer: {
-    auth
+    auth,
+    categories
   }
 });
