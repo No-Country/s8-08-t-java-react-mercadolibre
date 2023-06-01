96894702b83eab13338426de6c614c50561d7b7b
vic-ferr <victorhfp2014@gmail.com>
2023-05-31 23:59:10 -0400

    fix sale and slider product components-margin home

--- frontend/client/src/components/Sales/Sales.jsx
+++ frontend/client/src/components/Sales/Sales.jsx
@@ -1,32 +1,24 @@
 import React from "react";
-import ProductCard from "../Products/ProductCard/ProductCard";
-import FastSale from "./FastSale/FastSale";
+import SliderProduct from "../Products/SliderProduct/SliderProduct";
 
-const listSales = new Array(5).fill(null);
-const listHotSale = new Array(5).fill(null);
 const Sales = () => {
   return (
     <>
       <div className="">
-        <h3 className="text-base text-center  pb-4 pt-6">
+        <h3 className="text-base pb-4 pt-6">
           🔥 OFERTAS IMPERDIBLES 🔥{" "}
           <span className="text-ligthblue cursor-pointer"> ver mas...</span>
         </h3>
-        <div className="flex flex-wrap gap-5 justify-center min-h-[424px]">
-          {listSales?.map((produc, i) => {
-            return <ProductCard key={`${i}-sale`} />;
-          })}
+        <div className="min-h-[424px]">
+          <SliderProduct />
         </div>
-        <h3 className="text-base pb-4 pt-6 text-center"> OFERTAS CON ENVÍOS RÁPIDOS </h3>{" "}
-        <FastSale />
-        <h3 className="text-base text-center mt-6 pb-4 pt-6">
-          LO MEJOR DE ESTE HOT SALE ¡NO TE QUEDES SIN EL TUYO!{" "}
-          <span className="text-ligthblue cursor-pointer "> ver mas...</span>
+
+        <h3 className="text-base mt-6 pb-4 pt-6 uppercase">
+          ultimos productos cargados!
+          <span className="text-ligthblue cursor-pointer lowercase"> ver mas...</span>
         </h3>
-        <div className="flex flex-wrap gap-5 justify-center  min-h-[424px]">
-          {listHotSale?.map((produc, i) => {
-            return <ProductCard key={`${i}-hotsale`} />;
-          })}
+        <div className="min-h-[424px]">
+          <SliderProduct />
         </div>
       </div>
     </>
