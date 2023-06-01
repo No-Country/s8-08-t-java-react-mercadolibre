350c46a806ee21f661e6dbc19d6ed684eaedaddb
vic-ferr <victorhfp2014@gmail.com>
2023-06-01 01:27:45 -0400

    remove console and file listProduct.json

--- frontend/client/src/components/Products/SliderProduct/SliderProduct.jsx
+++ frontend/client/src/components/Products/SliderProduct/SliderProduct.jsx
@@ -8,12 +8,9 @@ import { Pagination } from "swiper";
 
 import { TiChevronRight } from "react-icons/ti";
 import { TiChevronLeft } from "react-icons/ti";
-
-import listProduct from "../../../utils/listProduct.json";
 import ProductCard from "../ProductCard/ProductCard";
 
 const SliderProduct = ({ products }) => {
-  console.log(products);
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseOver = () => {
@@ -64,8 +61,7 @@ const SliderProduct = ({ products }) => {
           }
         }}
         modules={[Pagination]}
-        className=""
-      >
+        className="">
         {products.map((product, index) => (
           <SwiperSlide onMouseOver={handleMouseOver} key={`${index}-card`}>
             <div className=" w-full flex justify-between">
@@ -78,14 +74,12 @@ const SliderProduct = ({ products }) => {
           <>
             <div
               className=" sm:flex items-center absolute top-[132px] bottom-0 right-0  cursor-pointer 
-  w-[50px] h-[50px] justify-center rounded-full z-[2]"
-            >
+  w-[50px] h-[50px] justify-center rounded-full z-[2]">
               <SlideNextButton />
             </div>
             <div
               className="hidden sm:flex items-center absolute top-[132px] bottom-0 left-0 cursor-pointer
-   w-[50px] h-[50px] justify-center rounded-full z-[8]"
-            >
+   w-[50px] h-[50px] justify-center rounded-full z-[8]">
               <SlidePrevButton />
             </div>
           </>
@@ -101,8 +95,7 @@ const SlideNextButton = () => {
   return (
     <button
       onClick={() => swiper.slideNext()}
-      className=" bg-white shadow-xl w-10 rounded-full h-10  flex items-center justify-center"
-    >
+      className=" bg-white shadow-xl w-10 rounded-full h-10  flex items-center justify-center">
       <TiChevronRight className=" text-[#4695fd] hover:text-[#4695fd] text-xl" />
     </button>
   );
@@ -114,8 +107,7 @@ const SlidePrevButton = () => {
   return (
     <button
       onClick={() => swiper.slidePrev()}
-      className=" bg-white shadow-xl w-10 rounded-full h-10 flex items-center justify-center"
-    >
+      className=" bg-white shadow-xl w-10 rounded-full h-10 flex items-center justify-center">
       <TiChevronLeft className=" text-[#4695fd] hover:text-[#4695fd] text-xl" />
     </button>
   );
