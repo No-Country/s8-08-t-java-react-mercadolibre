b6cc885c0dc2aeb8bc5a369a11d995001edf63e2
pablex72 <tonyp2hoy@gmail.com>
2023-06-01 16:35:19 -0400

    feature: adding mercado pago integration method payment that include pago facil rapipago and tarjet

---
+++ backend/src/main/java/com/nocountry/backend/dto/payment/MpPaymentDto.java
@@ -0,0 +1,17 @@
+package com.nocountry.backend.dto.payment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MpPaymentDto {
+
+    private String title;
+    private Integer quantity;
+    private float price;
+
+}
