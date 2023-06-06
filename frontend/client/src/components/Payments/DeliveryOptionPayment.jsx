d2cdf6adf22106c5fe88e4454aa154951c60e2d5
Franco Buceta <francobuceta95@gmail.com>
2023-06-06 12:07:27 -0300

    show address data in delivery option

--- frontend/client/src/components/Payments/DeliveryOptionPayment.jsx
+++ frontend/client/src/components/Payments/DeliveryOptionPayment.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SlLocationPin } from "react-icons/sl";
 import useMediaQuery from "../../hooks/useMediaQuery.js";
 import { useNavigate } from "react-router-dom";
@@ -8,8 +8,8 @@ import { postRequest } from "../../services/httpRequest.js";
 const DeliveryOptionPayment = () => {
   const navigate = useNavigate();
   const [selectedOption, setSelectedOption] = useState("");
+  const [localStorageData, setLocalStorageData] = useState(getLocalStorage("auth"));
   const isMobile = useMediaQuery("(max-width: 640px)");
-  const localStorageData = getLocalStorage("auth");
 
   const handleFirstChoice = () => {
     setSelectedOption("home delivery");
@@ -31,6 +31,17 @@ const DeliveryOptionPayment = () => {
     window.location.href = `https:${res.split(":")[2]}`;
   };
 
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setLocalStorageData(getLocalStorage("auth"));
+
+      window.addEventListener('storage', handleStorageChange);
+      return () => {
+        window.removeEventListener('storage', handleStorageChange);
+      };
+    };
+  }, []);
+
   return (
     <section className="bg-[#eeeeee] flex grow">
       <div className="mx-10 sm:mx-24 lg:ml-14 lg:mr-0 font-medium text-[#504A4A] w-full max-w-[825px]">
@@ -49,11 +60,25 @@ const DeliveryOptionPayment = () => {
                 className="text-[#3483FA] border rounded-full border-none bg-white p-[0.3rem] hidden sm:block"
                 fontSize={28}
               />
-              <span className="text-xs">
+              <p className="leading-4">
                 {localStorageData.user.address
-                  ? localStorageData.user.address.province.name
+                  ? (
+                    <>
+                      <span className="text-[15px] font-normal">
+                        {localStorageData.user.address.street}, {localStorageData.user.address.number}
+                      </span>
+                      <br />
+                      <span className="text-[13px] font-light text-[#737373]">
+                        C.P. {localStorageData.user.address.zip_code} - {localStorageData.user.address.province.name}, {localStorageData.user.address.locality}
+                      </span>
+                      <br />
+                      <span className="text-[13px] font-light text-[#737373]">
+                        {localStorageData.user.address.contact} - {localStorageData.user.address.phone}
+                      </span>
+                    </>
+                  )
                   : "Agregar domicilio"}
-              </span>
+              </p>
             </div>
 
             <div className="mr-12 ml-5 sm:ml-0">
@@ -72,9 +97,8 @@ const DeliveryOptionPayment = () => {
           </div>
 
           <div
-            className={`flex items-center justify-between w-full max-w-[825px] bg-[#FFFFFF] h-[60px] rounded mb-6 border-l-4 cursor-pointer ${
-              selectedOption === "home delivery" ? "border-ligthblue" : "border-transparent"
-            }`}
+            className={`flex items-center justify-between w-full max-w-[825px] bg-[#FFFFFF] h-[60px] rounded mb-6 border-l-4 cursor-pointer ${selectedOption === "home delivery" ? "border-ligthblue" : "border-transparent"
+              }`}
             onClick={handleFirstChoice}
           >
             <div className="flex items-center gap-12 ml-5 sm:ml-12">
@@ -84,7 +108,7 @@ const DeliveryOptionPayment = () => {
                 id="delivery"
                 value="option1"
                 checked={selectedOption === "home delivery"}
-                onChange={() => {}}
+                onChange={() => { }}
                 className="cursor-pointer hidden sm:block"
               />
               <span className="text-xs">Llega mañana a tu domicilio</span>
@@ -100,9 +124,8 @@ const DeliveryOptionPayment = () => {
           </div>
 
           <div
-            className={`flex items-center justify-between w-full max-w-[825px] bg-[#FFFFFF] h-[60px] rounded mb-6 border-l-4 cursor-pointer ${
-              selectedOption === "mail delivery" ? "border-ligthblue" : "border-transparent"
-            }`}
+            className={`flex items-center justify-between w-full max-w-[825px] bg-[#FFFFFF] h-[60px] rounded mb-6 border-l-4 cursor-pointer ${selectedOption === "mail delivery" ? "border-ligthblue" : "border-transparent"
+              }`}
             onClick={handleSecondChoice}
           >
             <div className="flex items-center gap-12 ml-5 sm:ml-12">
@@ -112,7 +135,7 @@ const DeliveryOptionPayment = () => {
                 id="delivery"
                 value="option2"
                 checked={selectedOption === "mail delivery"}
-                onChange={() => {}}
+                onChange={() => { }}
                 className="cursor-pointer hidden sm:block"
               />
               <span className="text-xs">Retiro en correo y otros puntos</span>
