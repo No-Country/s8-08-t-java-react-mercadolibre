539c3db93f1827322f70e9754780f25d7e8c8d87
Dario Elguero <darioelguero@gmail.com>
2023-06-09 19:39:55 -0300

    changed config in SpringDocConfig

--- backend/src/main/java/com/nocountry/backend/config/SpringDocConfig.java
+++ backend/src/main/java/com/nocountry/backend/config/SpringDocConfig.java
@@ -9,8 +9,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(servers = {
-        @Server(url = "{host}", variables = @ServerVariable(name = "host", defaultValue = "https://s8-08-t-java-react-mercadolibre-production.up.railway.app/", allowableValues = {
-                "https://s8-08-t-java-react-mercadolibre-production.up.railway.app/", "http://localhost:8080"})
+        @Server(url = "{host}", variables = @ServerVariable(name = "host", defaultValue = "https://mercadolibreclon-production.up.railway.app/", allowableValues = {
+                "https://mercadolibreclon-production.up.railway.app/", "http://localhost:8080"})
 
                 , description = "Host name by environment")})
 @SecurityScheme(type = SecuritySchemeType.HTTP, name = "bearerAuth", scheme = "bearer", bearerFormat = "JWT")
